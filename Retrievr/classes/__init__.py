5b2456a2d46b7268f27016ef55386fde6b9896ec
Chris Rupert <christopherjrupert@gmail.com>
2020-09-12 20:21:38 -0300

    looking for help outputting werkzeug to log file

--- Retrievr/classes/__init__.py
+++ Retrievr/classes/__init__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
-    from flask import Flask
-    from logging.handlers import TimedRotatingFileHandler
-    import os
-    from datetime import datetime
-    import logging
+from flask import Flask
+from logging.handlers import TimedRotatingFileHandler
+import os
+from datetime import datetime
+import logging
 
-    app = Flask(__name__)
-    app.config.from_object('Retrievr.config.Config')
+app = Flask(__name__)
+app.config.from_object('Retrievr.config.Config')
 
-    LOG_PATH = os.path.join(os.getcwd(), "%s" % app.config.get('LOG_PATH'))
+LOG_PATH = os.path.join(os.getcwd(), "%s" % app.config.get('LOG_PATH'))
 
-    logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s: %(message)s")
-    handler = TimedRotatingFileHandler('%s/Retrievr-%s.log' % (LOG_PATH,
-                                                            datetime.now().strftime("%Y-%m-%d")),
-                                    when='midnight',
-                                    backupCount=30)
-    handler.setLevel(logging.DEBUG if app.config.get('DEBUG') else logging.INFO)
-    handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s"))
-    app.logger.setLevel(logging.DEBUG if app.config.get('DEBUG') else logging.INFO)
-    app.logger.addHandler(handler)
+logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+handler = TimedRotatingFileHandler('%s/Retrievr-%s.log' % (LOG_PATH,
+                                                        datetime.now().strftime("%Y-%m-%d")),
+                                when='midnight',
+                                backupCount=30)
+handler.setLevel(logging.DEBUG if app.config.get('DEBUG') else logging.INFO)
+handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s"))
+app.logger.setLevel(logging.DEBUG if app.config.get('DEBUG') else logging.INFO)
+app.logger.addHandler(handler)
 
 from . import user
 from . import artist
